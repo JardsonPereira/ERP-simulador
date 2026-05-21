f63b683e3fd204146848815b5924fd237dea93fc
Jardson Pereira <jardsonspereira81@gmail.com>
2026-05-21 10:08:44 -0400

    Update 04_DRE.py

--- pages/04_DRE.py
+++ pages/04_DRE.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import get_data_cached, check_auth, inject_css, gerar_relatorio_pdf
 
 check_auth(); inject_css()
+supabase = get_supabase()
 st.header("📈 DRE")
 lancamentos = get_data_cached("lancamentos", st.session_state.user.id)
 contas = get_data_cached("contas", st.session_state.user.id)
